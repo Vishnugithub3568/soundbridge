1b2d418216845e261fb94d172977ccf55fcae99c
Vishnugithub3568 <u.vishnu3568@gmail.com>
2026-03-10 13:27:47 +0530

    Initial SoundBridge project setup and folder structure

---
+++ soundbridge/src/com/migrator/Main.java
@@ -0,0 +1,9 @@
+package com.migrator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SOUNDBRIDGE");
+        System.out.println("Spotify → YouTube Migration Tool");
+        System.out.println("System starting...");
+    }
+}
